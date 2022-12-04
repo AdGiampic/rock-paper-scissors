2311d8f9d82a31dd5142850c6c14bf67bf8f2d60
Unknown-Drifter <adgiampic@gmail.com>
2022-12-04 10:08:21 +0100

    Created index.html and script.js, script.js will store the logic of the game

---
+++ script.js
@@ -0,0 +1,10 @@
+const Rock = "Rock";
+const Paper = "Paper";
+const Scissors = "Scissors";
+
+// selezionare una delle stringhe 
+// usare random da 1 a 3 per prendere una delle stringhe
+
+function getComputerChoice () {
+    return 
+}
